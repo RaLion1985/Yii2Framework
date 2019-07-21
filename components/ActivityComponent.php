eec3b8e247a9b72664738075806981395fff6af1
RaLion1985 <y-shkuro@mail.ru>
2019-07-21 08:56:35 +0300

    Д.з. по 2 уроку

---
+++ components/ActivityComponent.php
@@ -0,0 +1,30 @@
+<?php
+
+
+namespace app\components;
+
+
+use yii\base\Component;
+
+class ActivityComponent extends Component
+{
+    public $classEntity;
+
+    public function init(){
+        parent::init();
+        if (empty($this->classEntity)){
+            throw new \Exception('ClassEntity param required');
+        }
+    }
+
+    public function getEntity (){
+        return new $this->classEntity();
+    }
+    public function createActivity(&$model): bool
+    {
+        if ($model->validate()){
+            return true;
+        }
+        return false;
+    }
+}
